8882fcff352a0201224ce5002d99caa9018a27c8
高宗凯 <asgzk@163.com>
2018-03-29 22:59:55 +0800

    delete myjava

--- src/Fenzi.java
+++ src/Fenzi.java
@@ -1,6 +1,11 @@
-
+// 1+1/2+//3...+//99=
 public class Fenzi {
 
+	public static void main(String args[]) {
+		
+		Fenzi fz = new Fenzi();
+		fz.add();
+	}
 	void add() {
 		double sum = 0.0;
 		for (int i = 1;i < 100;i++) {
